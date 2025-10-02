53b65cc18be52b9a06e3cfc51d4a86307276a164
Yeamin Foysal <yeaminfoysal14@gmail.com>
2025-10-03 00:23:39 +0600

    not, scalar functions complete

---
+++ NOT_Scalar_Functions.sql
@@ -0,0 +1,36 @@
+ -- select students who are not from uk and usa
+SELECT
+  *
+FROM
+  students
+where
+  country != 'UK'
+  and country != 'USA';
+
+SELECT
+  *
+FROM
+  students
+where
+  not country = 'UK'
+  and country = 'USA';
+
+-- apply upper function 
+SELECT
+  upper(first_name)
+FROM
+  students;
+
+-- apply concat function 
+SELECT
+  concat(first_name, ' ', last_name) as "Full Name"
+FROM
+  students;
+
+-- apply length function 
+SELECT
+  length(first_name) as "First Name's Length",
+  first_name
+FROM
+  students
+  order by "First Name's Length" desc;
